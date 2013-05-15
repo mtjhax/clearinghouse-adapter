ce859835a75f10e39aa7419faa52666fd127562e
mtjhax <mtj@cmpj.org>
2013-05-14 20:29:36 -0400

    refactored import code to handle multiple files and collect error status for each

--- lib/adapter_sync.rb
+++ lib/adapter_sync.rb
@@ -40,17 +40,53 @@ begin
   apiconfig['raw'] = false
   clearinghouse = ApiClient.new(apiconfig)
 
+  # process for syncing the provider scheduling system with the clearinghouse:
+  #
+  # 1. adapter polls local system for new/modified tickets, pushes new tickets/changes
+  #
+  # how does the adapter know they are new/modified? it needs a local cache to list them as already having been shared,
+  # possibly just using date of last update, or if the cache is empty it needs to look them up on the CH (or just try creating them)
+  #
+  # if possible, mark the ticket in the provider system as having been shared, but don't rely on these marks
+  #
+  # 2. adapter polls local system for claimed tickets with modifications/results, pushes results/changes
+  #
+  # how does the adapter know which tickets are claimed by the provider? use local cache of claimed tickets,
+  # or query CH for all claimed ticket IDs.
+  #
+  # 3. adapter polls CH for claims and results on originated tickets, pulls claims/results
+  #
+  # primary point of this is to mark local ticket (if possible) so dispatcher is aware it has been claimed/completed
+  # although there is a fallback which is the CH lets them know,
+  #
+  # adapter recognizes new claims/results on originated tickets by date(?)
+  #
+  # 4. adapter polls CH for new and modified claimed tickets, pulls claimed tickets/changes
+  #
+  # how does adapter know which claimed tickets are new or changed? cache, fallback is to query all claimed
+  # tickets and see which ones exist on local system already.
+
+  # How do we know which tickets and changes are new? Local cache or marked in database? We should try to avoid
+  # stashing Adapter-specific information in the provider system database because we don't really have control
+  # over it, so we will stash them locally, only after successful push.
+
+
   importer = Import.new(logger)
-  importer.import_folder(options[:import][:new_folder]) do |row|
+  import_results = importer.import_folder(options[:import][:new_folder]) do |row|
     begin
       # TODO allow API to throw exceptions on server error responses or force it to return a result?
       # TODO we need to cleanly handle when we post a trip ticket that already exists
-      # TODO report errors to monitor in case they require a notification to user (handle notifications in one place?)
       result = clearinghouse.post(:trip_tickets, row)
       logger.info "Posted trip ticket to API, result #{result}"
     rescue Exception => e
       logger.error "Row error: " << e.message
     end
+  end
+
+  # TODO send notification for files that failed (handled here or by Monitor service?)
+  logger.info "Imported #{import_results.length} files, results:"
+  import_results.each do |r|
+    logger.info "File [#{r[:file_name]}], Rows [#{r[:rows]}], Error [#{r[:error] ? r[:error_msg] : 'none'}], "
   end
 
 rescue Exception => e
--- lib/import.rb
+++ lib/import.rb
@@ -5,8 +5,11 @@ require 'active_support/core_ext/hash/indifferent_access'
 # :new_folder => 'tmp/import'
 # :done_folder => 'tmp/import_done'
 #
-# if passed a block, will call each block with a hash of row values
+# if import passed a block, will call each block with a hash of row values
 # if not passed a block, will return CSV::Table
+#
+# import_folder requires a block to handle rows, returns array of hashes containing:
+# file_name: string, rows: integer, error: true/false, error_msg: string
 
 class Import
   def initialize(logger, options = {})
@@ -20,34 +23,50 @@ class Import
 
   def import(file)
     log "Importing #{file}"
-    # TODO capture exceptions, particularly CSV::MalformedCSVError
     csv = CSV.open(file, headers: true, return_headers: false)
+    data = csv.read
     if block_given?
-      csv.each do |row|
+      log "Importing #{data.length} rows"
+      row_count = 0
+      data.each do |row|
         row_hash = Hash[row.headers.zip(row.fields)]
         log "Row: #{row}"
         yield(row_hash)
+        row_count += 1
       end
+      row_count
     else
-      data = csv.read
-      log "Returning #{data.length} imported rows to caller"
+      data
     end
   end
 
   def import_folder(import_dir, &row_handler)
+    results = []
     if import_dir.nil?
       log "Import folder not configured, will not check for files to import"
     elsif Dir[import_dir].empty?
       log "Import folder #{import_dir} does not exist, will not check for files to import"
     else
+      # TODO create import log in the import folder, as well as log overall import result to main log
+      # TODO log errors, return files, error counts to main code to be logged and for notification
+      # TODO if import fails, stash name and timestamp to avoid reimporting that file
+      # TODO if there is an output folder, move file there when finished
+
       log "Checking folder #{File.expand_path(import_dir)} for files to import"
       import_files = Dir[File.join(File.expand_path(import_dir), "*.{txt,csv}")]
       log "Found #{import_files.length} files to import"
+
       import_files.each do |file|
-        import(file) do |row|
-          row_handler.call(row)
+        begin
+          row_count = import(file){ |row| row_handler.call(row) }
+          results << { file_name: file, rows: row_count, error: false }
+        rescue CSV::MalformedCSVError, SystemCallError => e
+          # handles CSV parsing errors and file IO errors
+          log "Error: #{e.to_s}"
+          results << { file_name: file, error: true, error_msg: e.to_s }
         end
       end
     end
+    results
   end
 end
